948edc6c2b0c91a73fe0cb428384e04eca3218f3
mdmasum-shuvo <mdmasum.shuvo.cse@gmail.com>
2022-01-19 18:07:50 +0600

    c

---
+++ lib/models/CartItem.dart
@@ -0,0 +1,13 @@
+
+class CartItem {
+  final String id;
+  final String productTitle;
+  final int productQuantity;
+  final double productPrice;
+
+  CartItem(
+      {required this.id,
+        required this.productTitle,
+        required this.productQuantity,
+        required this.productPrice});
+}
---
+++ lib/models/Order.dart
@@ -0,0 +1,15 @@
+
+import 'CartItem.dart';
+
+class Order {
+  final String orderId;
+  final double amount;
+  final List<CartItem> products;
+  final DateTime dateTime;
+
+  Order(
+      {required this.orderId,
+        required this.amount,
+        required this.products,
+        required this.dateTime});
+}
